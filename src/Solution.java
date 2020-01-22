9582be99adc876d9ee01ac2613d92b92e34fa46b
Wweakness <770199484@qq.com>
2020-01-22 23:59:55 +0800

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int myAtoi(String str) {
+        char[] s=str.toCharArray();
+        StringBuffer ss=new StringBuffer();
+       for(int i=0;i<s.length;i++){
+           if(s[i]!=' '){
+               
+           }
+       }
+    }
+}
